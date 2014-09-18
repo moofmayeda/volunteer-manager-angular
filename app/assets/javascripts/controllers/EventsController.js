2fb896cdcba5e9b5b9a33b8c5bdd3176f34b31dc
Ellen Krause Lehman & moof mayeda <student@epicodus.com>
2014-09-18 16:57:45 -0700

    WIP add event functionality

--- app/assets/javascripts/controllers/EventsController.js
+++ app/assets/javascripts/controllers/EventsController.js
@@ -1,7 +1,13 @@
 volunteerManager.controller('EventsController', function EventsController($scope, EventsFactory) {
   $scope.EventsFactory = EventsFactory;
   $scope.events = EventsFactory.events;
-
+  $scope.addEvent = function() {
+    alert("hi")
+  };
+  $scope.submit = function() {
+    $scope.addEvent();
+    $scope.new_event = false
+  }
   $scope.getEvents = (function() {
     EventsFactory.getEvents()
       .success(function(data) {
