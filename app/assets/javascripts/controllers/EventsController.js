349050a6402eba52e7ba577409127923a052778a
Ellen Krause Lehman & moof mayeda <student@epicodus.com>
2014-09-18 16:31:19 -0700

    display events retrieved from database

--- app/assets/javascripts/controllers/EventsController.js
+++ app/assets/javascripts/controllers/EventsController.js
@@ -1,4 +1,12 @@
 volunteerManager.controller('EventsController', function EventsController($scope, EventsFactory) {
-  $scope.events = EventsFactory.events;
   $scope.EventsFactory = EventsFactory;
-});
+  $scope.events = EventsFactory.events;
+
+  $scope.getEvents = (function() {
+    EventsFactory.getEvents()
+      .success(function(data) {
+        $scope.events = data.events;
+      })
+  })();
+
+  });
--- app/assets/javascripts/services/EventsFactory.js
+++ app/assets/javascripts/services/EventsFactory.js
@@ -1,6 +1,14 @@
-volunteerManager.factory('EventsFactory', function EventsFactory() {
+volunteerManager.factory('EventsFactory', function EventsFactory($http) {
   var factory = {};
-  factory.events = [{"name": 'Feed the Programmers', "location": 'Epicodus', "date": '2014-09-18'}, {"name":"Ellen's Birthday", "location": "Ellen's House", "date": "2014-05-31"}];
+  // factory.events = [{"name": 'Feed the Programmers', "location": 'Epicodus', "date": '2014-09-18'}, {"name":"Ellen's Birthday", "location": "Ellen's House", "date": "2014-05-31"}];
+
+
+  factory.events = [{"name":"fake event"}]
+  factory.getEvents = function () {
+    return $http.get('/events.json')
+  };
+
+  // factory.getEvents();
 
   return factory;
 })
