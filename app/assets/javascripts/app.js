1b48942168e6b1df9222347d94f2f9335930d83b
Ellen Krause Lehman & moof mayeda <student@epicodus.com>
2014-09-22 13:55:31 -0700

    remove sample ui router files

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -1,34 +1,11 @@
 var volunteerManager = angular.module('volunteerManager', ['templates', 'ui.router']);
 
 volunteerManager.config(function ($stateProvider, $urlRouterProvider) {
-  $urlRouterProvider.otherwise("/state1");
+  $urlRouterProvider.otherwise("/events");
 
    $stateProvider
-     .state('state1', {
-       url: "/state1",
-       templateUrl: "state1.html"
-     })
-     .state('state1.list', {
-       url: "/list",
-       templateUrl: "state1.list.html",
-       controller: function($scope) {
-         $scope.items = ["A", "List", "Of", "Items"];
-       }
-     })
-     .state('state2', {
-       url: "/state2",
-       templateUrl: "state2.html"
-     })
-     .state('state2.list', {
-       url: "/list",
-       templateUrl: "state2.list.html",
-       controller: function($scope) {
-         $scope.things = ["A", "Set", "Of", "Things"];
-       }
-     })
-
      .state('events.detail', {
-      url: "/events/:id",
+      url: "/:id",
       templateUrl: "Events.detail.html",
       controller: "EventController"
      })
